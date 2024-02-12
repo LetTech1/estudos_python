6c599dcb550f98089de05b9d140918ba5516213b
LeticiaBapFelix <leticiafelix2799@gmail.com>
2024-02-12 20:22:01 -0300

    Iniciando curso de python da Geek University

---
+++ pep8.py
@@ -0,0 +1,26 @@
+""" import this
+
+É um poema criado por Tim Peters, que é dev python e poeta 
+
+PEP8 significa Python Enhancement Proposals (Propostas de Aprimoramento do Python)
+A ideia do PEP8 é que possamos escrever códigos Python de forma mais Pythônica
+
+Aqui estão algumas dessas boas práticas contidas no PEP8:
+
+1 - Utiliza Camel Case para nomes de classes
+
+2 - Utilize nomes em minúsculo, separados por underline para funções ou variáveis
+
+3 - Utilize 4 espaços para identação (Não utilizar tab, pois a quantidade de espaços pode variar dependendo da máquina)
+
+4 - Linhas em branco (É necessário 2 linhas em branco a partir de um texto para criar 
+classes e métodos dentro de uma classe, devem ser separados por uma linha em branco)
+
+5 - Imports devem ser sempre feitos em linhas separadas
+  - Imports devem ser colocados no topo do arquivo, logo depois de quaisquer comentários ou docstrings e antes de constante ou variáveis globais
+
+6 - Espaços em expressões e instruções
+
+7 - Termine sempre uma instrução com uma nova linha
+
+"""
